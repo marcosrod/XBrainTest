3d85be6fbc67b17b60797a3b66284d45638dffb9
marcosrod <marcos-rodrigo05@hotmail.com>
2021-09-01 13:48:45 -0300

    Feat: Seller Save and GetById with H2 Persistence

--- sales/src/main/java/com/xbrainapi/sales/controller/SellerController.java
+++ sales/src/main/java/com/xbrainapi/sales/controller/SellerController.java
@@ -1,12 +1,38 @@
 package com.xbrainapi.sales.controller;
 
+import com.xbrainapi.sales.model.Seller;
+import com.xbrainapi.sales.repository.SellerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/sellers")
 public class SellerController {
 
+    private SellerRepository sellerRepository;
 
+    public SellerController(SellerRepository sellerRepository){
+        super();
+        this.sellerRepository = sellerRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Seller> saveSeller(@RequestBody Seller seller){
+        sellerRepository.save(seller);
+        return new ResponseEntity<>(seller, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Optional<Seller>> getSellerById(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok().body(sellerRepository.findById(id));
+        } catch (NoSuchElementException nsee) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
 }
